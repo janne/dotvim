7029a9a970f8de7f361bdfdc37d3e8a42fcc190d
Jan Andersson <jan.andersson@travelpartner.se>
2010-04-12 13:02:47 +0200

    Initial import (exported from dotfiles repo)

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ln -sfh `pwd`/`dirname $0`/vim ~/.vim
+ln -sfh `pwd`/`dirname $0`/vimrc ~/.vimrc
+ln -sfh `pwd`/`dirname $0`/gvimrc ~/.gvimrc
---
+++ vim/bundle/markdown.vim/ftdetect/markdown.vim
@@ -0,0 +1,1 @@
+au BufRead,BufNewFile *.md,*.mkd,*.txt setlocal filetype=markdown
